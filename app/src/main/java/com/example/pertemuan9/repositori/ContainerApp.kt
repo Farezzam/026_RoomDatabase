92bdf94d34596d300b21e5618a5d3dc816d530d7
ASUS <faris13abiyu@gmail.com>
2025-11-28 20:04:49 +0700

    isi ContainerApp.kt (repositori)

--- app/src/main/java/com/example/pertemuan9/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/pertemuan9/repositori/ContainerApp.kt
@@ -1,2 +1,24 @@
 package com.example.pertemuan9.repositori
 
+import android.app.Application
+import android.content.Context
+
+interface ContainerApp {
+    val repositoriSiswa : RepositoriSiswa
+}
+
+class ContainerDataApp(private val context: Context):
+        ContainerApp{
+        override val repositoriSiswa: RepositoriSiswa by lazy{
+            OfflineRepositoriSiswa(
+                siswaDao = DatabaseSiswa.getDatabase(context).siswaDao())
+        }
+        }
+
+class AplikasiSiswa : Application(){
+    lateinit var container: ContainerApp
+    override fun onCreate() {
+        super.onCreate()
+        container = ContainerDataApp(context = this)
+    }
+}
