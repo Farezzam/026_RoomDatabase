751a5ba5c221f1d91c13ebb7d3c1c7802b4d297d
ASUS <faris13abiyu@gmail.com>
2025-11-29 00:18:25 +0700

    Edit DestinasiHome.kt

--- app/src/main/java/com/example/pertemuan9/view/route/DestinasiHome.kt
+++ app/src/main/java/com/example/pertemuan9/view/route/DestinasiHome.kt
@@ -2,7 +2,7 @@ package com.example.pertemuan9.view.route
 
 import com.example.pertemuan9.R
 
-object DestinasiEntry: DestinasiNavigasi {
+object DestinasiHome: DestinasiNavigasi {
     override val route = "home"
     override val titleRes = R.string.entry_siswa
 }
